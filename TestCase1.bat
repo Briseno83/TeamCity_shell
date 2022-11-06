93db9762d5dd6d5a499d467031153d4407654011
Briseno83 <91988122+Briseno83@users.noreply.github.com>
2022-11-05 22:10:07 -0400

    Gmail access
    Gmail access using a *.bat file

---
+++ TestCase1.bat
@@ -0,0 +1,105 @@
+@if (@CodeSection == @Batch) @then
+
+@echo off
+
+rem Use %SendKeys% to send keys to the keyboard buffer
+set SendKeys=CScript //nologo //E:JScript "%~F0"
+START CHROME "https://accounts.google.com/v3/signin/identifier?dsh=S1460456777%3A1667698075109603&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAtkEiTGLH4nR-ZmPvbvY8ZIdaEXH-9K3qqHU6IKS5di966VwZ-XAqjnJTVb4AzveoIZAuHh"
+
+timeout /t 3
+%SendKeys% "teamcity.matrix@gmail.com"
+%SendKeys% "{ENTER}"
+timeout /t 3
+%SendKeys% "Matrix2022xX#"
+%SendKeys% "{ENTER}"
+timeout /t 4
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{ENTER}"
+timeout /t 2
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{TAB}"
+%SendKeys% "{ENTER}"
+
+timeout /t 4
+TASKKILL /IM chrome.exe /F
+
+goto :EOF
+
+@end
+// JScript section
+
+var WshShell = WScript.CreateObject("WScript.Shell");
+WshShell.SendKeys(WScript.Arguments(0));

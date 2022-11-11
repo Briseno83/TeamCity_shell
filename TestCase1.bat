a88d13a8be36bd4e8b94430a3216f75fb8311a78
Briseno83 <91988122+Briseno83@users.noreply.github.com>
2022-11-11 12:41:26 -0500

    Add files via upload

--- TestCase1.bat
+++ TestCase1.bat
@@ -5,23 +5,23 @@
 set SendKeys=CScript //nologo //E:JScript "%~F0"
 START CHROME --restore-last-session --start-fullscreen "https://accounts.google.com/v3/signin/identifier?dsh=S1460456777%3A1667698075109603&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAtkEiTGLH4nR-ZmPvbvY8ZIdaEXH-9K3qqHU6IKS5di966VwZ-XAqjnJTVb4AzveoIZAuHh"
 
-timeout /t 5
+ping -n 10 127.0.0.1 >NUL
 %SendKeys% "teamcity.matrix@gmail.com"
 %SendKeys% "{ENTER}"
-timeout /t 4
+ping -n 10 127.0.0.1 >NUL
 %SendKeys% "Matrix2022xX#"
 %SendKeys% "{ENTER}"
-timeout /t 4
+ping -n 10 127.0.0.1 >NUL
 FOR /L %%G IN (1,1,7) DO %SendKeys% "{TAB}"
 %SendKeys% "{ENTER}"
-timeout /t 3
+ping -n 10 127.0.0.1 >NUL
 FOR /L %%G IN (1,1,11) DO %SendKeys% "{TAB}" %SendKeys% "{TAB}"
 %SendKeys% "{ENTER}"
-timeout /t 6
+ping -n 10 127.0.0.1 >NUL
 FOR /L %%G IN (1,1,4) DO %SendKeys% "{TAB}" %SendKeys% "{TAB}"
 %SendKeys% "{ENTER}"
 
-timeout /t 7
+:timeout /t 7
 ::Get-Process chrome | ForEach-Object { $_.CloseMainWindow() | Out-Null}
 taskkill /IM chrome.exe /F
 

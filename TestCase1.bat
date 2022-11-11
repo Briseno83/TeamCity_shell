b4709ef706553021810ed326e2514567b6c92c02
Briseno83 <91988122+Briseno83@users.noreply.github.com>
2022-11-11 12:36:16 -0500

    Add files via upload

--- TestCase1.bat
+++ TestCase1.bat
@@ -2,9 +2,8 @@
 
 @echo off
 
-rem Use %SendKeys% to send keys to the keyboard buffer
 set SendKeys=CScript //nologo //E:JScript "%~F0"
-START CHROME "https://accounts.google.com/v3/signin/identifier?dsh=S1460456777%3A1667698075109603&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAtkEiTGLH4nR-ZmPvbvY8ZIdaEXH-9K3qqHU6IKS5di966VwZ-XAqjnJTVb4AzveoIZAuHh"
+START CHROME --restore-last-session --start-fullscreen "https://accounts.google.com/v3/signin/identifier?dsh=S1460456777%3A1667698075109603&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAtkEiTGLH4nR-ZmPvbvY8ZIdaEXH-9K3qqHU6IKS5di966VwZ-XAqjnJTVb4AzveoIZAuHh"
 
 timeout /t 5
 %SendKeys% "teamcity.matrix@gmail.com"
@@ -18,12 +17,14 @@ FOR /L %%G IN (1,1,7) DO %SendKeys% "{TAB}"
 timeout /t 3
 FOR /L %%G IN (1,1,11) DO %SendKeys% "{TAB}" %SendKeys% "{TAB}"
 %SendKeys% "{ENTER}"
-timeout /t 8
+timeout /t 6
 FOR /L %%G IN (1,1,4) DO %SendKeys% "{TAB}" %SendKeys% "{TAB}"
 %SendKeys% "{ENTER}"
 
 timeout /t 7
-TASKKILL /IM chrome.exe /F
+::Get-Process chrome | ForEach-Object { $_.CloseMainWindow() | Out-Null}
+taskkill /IM chrome.exe /F
+
 
 goto :EOF
 
